b9ef3f0910c31dc9303608c015ff677cace7faa4
Boudnaoui <emboudnaoui@cmaisonneuve.qc.ca>
2023-03-22 15:33:16 -0400

    ajout du swipe effect sur les divs a swipe

--- MagicMenage/scripts/main.js
+++ MagicMenage/scripts/main.js
@@ -229,7 +229,7 @@ window.onload = function () {
 
         let dots = document.querySelectorAll('.dot-container span');
         let divs = document.querySelectorAll('.flexCustomer > div');
-        let currentDiv = 0;
+        let currentDiv = 0, startXClient = 0, endXClient = 0;
 
         dots.forEach((dot, index) => {
             dot.addEventListener('click', () => {
@@ -242,13 +242,42 @@ window.onload = function () {
             });
         });
 
+        divs.forEach((div, index) => {
+            div.addEventListener('touchstart', (event) => {
+                startXClient = event.touches[0].clientX;
+            });
+
+            div.addEventListener('touchend', (event) => {
+                endXClient = event.changedTouches[0].clientX;
+
+                if (startXClient > endXClient) {
+                    dots[currentDiv].classList.remove('active');
+                    divs[currentDiv].style.display = 'none';
+
+                    currentDiv = (currentDiv + 1) % divs.length;
+                    dots[currentDiv].classList.add('active');
+                    divs[currentDiv].style.display = 'flex';
+                }
+                else if (startXClient < endXClient) {
+                    dots[currentDiv].classList.remove('active');
+                    divs[currentDiv].style.display = 'none';
+
+                    currentDiv = (currentDiv - 1 + divs.length) % divs.length;
+                    dots[currentDiv].classList.add('active');
+                    divs[currentDiv].style.display = 'flex';
+                }
+            });
+        });
+
+
+
+
         let dotService = document.querySelectorAll('.dot span');
         let services = document.querySelectorAll('.flexServices > div');
-        let index = 0;
+        let index = 0, startX = 0, endX = 0;
 
         for (let i = 0; i < dotService.length; i++) {
             dotService[i].addEventListener('click', () => {
-                console.log(i);
                 dotService[index].classList.remove('active');
                 services[index].style.display = 'none';
                 index = i;
@@ -257,7 +286,29 @@ window.onload = function () {
             });
         }
 
+        for (let i = 0; i < services.length; i++) {
+            services[i].addEventListener('touchstart', (event) => {
+                startX = event.touches[0].clientX;
+            });
 
+            services[i].addEventListener('touchend', (event) => {
+                endX = event.changedTouches[0].clientX;
 
+                if (startX > endX) {
+                    dotService[index].classList.remove('active');
+                    services[index].style.display = 'none';
+                    index = (index + 1) % services.length;
+                    dotService[index].classList.add('active');
+                    services[index].style.display = 'flex';
+                }
+                else if (startX < endX) {
+                    dotService[index].classList.remove('active');
+                    services[index].style.display = 'none';
+                    index = (index - 1 + services.length) % services.length;
+                    dotService[index].classList.add('active');
+                    services[index].style.display = 'flex';
+                }
+            });
+        }
     }
 }    
